fe0e7ed6265dc3024d3395015752bfb606e3571a
AsareelDon <asareeldonpena@gmail.com>
2024-09-18 11:11:09 +0800

    feat: added service interface

---
+++ DockerizeProject/webservices/src/main/java/com/stockflow/webservices/services/UserServices.java
@@ -0,0 +1,14 @@
+package com.stockflow.webservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.stockflow.webservices.models.Users;
+
+public interface UserServices {
+    Users createUsers(Users newUsers);
+    List<Users> getListOfUsers();
+    Optional<Users> findUserById(Long userId);
+    Users updateUserDetails(Users updatedUssers, Long id);
+    void deactivateUser(Long id);
+}
