f365f3be0db0574aed326ef90fd167ba8d6ea168
AsareelDon <asareeldonpena@gmail.com>
2024-09-19 13:28:55 +0800

    feat: added validation error message for requestDTO

--- DockerizeProject/webservices/src/main/java/com/stockflow/webservices/dto/UserAccountRequestDTO.java
+++ DockerizeProject/webservices/src/main/java/com/stockflow/webservices/dto/UserAccountRequestDTO.java
@@ -1,13 +1,18 @@
 package com.stockflow.webservices.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserAccountRequestDTO {
     private Long userId;
+    @NotBlank(message = "First name is required.")
     private String firstname;
     private String middlename;
+    @NotBlank(message = "Last name is required.")
     private String lastname;
+    @NotBlank(message = "Email address is required.")
     private String userEmail;
+    @NotBlank(message = "Password is required.")
     private String userPassword;
 }
