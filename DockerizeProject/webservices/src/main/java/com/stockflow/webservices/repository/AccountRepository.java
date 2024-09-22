2fc28dca61b591cf62a9211cf7a2f113304f5c03
AsareelDon <asareeldonpena@gmail.com>
2024-09-22 18:02:14 +0800

    feat: added username search method in repo

--- DockerizeProject/webservices/src/main/java/com/stockflow/webservices/repository/AccountRepository.java
+++ DockerizeProject/webservices/src/main/java/com/stockflow/webservices/repository/AccountRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.stockflow.webservices.models.Accounts;
 
 public interface AccountRepository extends JpaRepository<Accounts, Long> {
+    Accounts findByUserName(String userName);
 }
