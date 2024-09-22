567bfee7da6fb321f9a7e156ecd3dc1461d1bee6
AsareelDon <asareeldonpena@gmail.com>
2024-09-22 17:56:03 +0800

    feat: added new service for password validation

--- DockerizeProject/webservices/src/main/java/com/stockflow/webservices/services/PasswordServices.java
+++ DockerizeProject/webservices/src/main/java/com/stockflow/webservices/services/PasswordServices.java
@@ -2,4 +2,6 @@ package com.stockflow.webservices.services;
 
 public interface PasswordServices {
     public String passwordEncryption(String userPassword);
+
+    public boolean validateUserPassword(String hash, String userPassword);
 }
