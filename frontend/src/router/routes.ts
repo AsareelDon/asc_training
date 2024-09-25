14ddc04004d0384193f667bcb35959b59991e3a9
AsareelDon <asareeldonpena@gmail.com>
2024-09-25 12:01:18 +0800

    feat: added route for login and register

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -4,8 +4,12 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+    ],
   },
+  { path: '/login', component: () => import('pages/LoginPage.vue') },
+  { path: '/register', component: () => import('pages/RegistrationPage.vue') },
 
   // Always leave this as last one,
   // but you can also remove it
