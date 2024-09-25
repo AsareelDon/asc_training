41c23f4840fe1e4e54d5de55b8335c77ce34477e
AsareelDon <asareeldonpena@gmail.com>
2024-09-25 12:01:04 +0800

    feat: added registration page with form component

---
+++ frontend/src/pages/RegistrationPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page padding>
+        <h3>Signup here Page</h3>
+        <FormComponent @submit="handleFormSubmit"/>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import FormComponent from 'src/components/FormComponent.vue';
+
+export default defineComponent({
+  components: {
+    FormComponent
+  },
+
+  setup() {
+    const handleFormSubmit = (forms: {
+      firstName: string;
+      middleName: string;
+      lastName: string;
+      userEmail: string;
+      userPassword: string;
+    }) => {
+      console.log('Credentials: ', forms);
+    };
+
+    return {
+      handleFormSubmit
+    };
+  }
+});
+</script>
